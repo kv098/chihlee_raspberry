dc252db34c45a84fa59c5da32ee859345dc35f56
kv098 <gao83017@gmail.com>
2024-08-17 15:13:12 +0800

    modify

--- 2024_08_17/receive.py
+++ 2024_08_17/receive.py
@@ -1,7 +1,10 @@
 import paho.mqtt.client as mqtt
 import redis 
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-redis_conn = redis.Redis(host='localhost',port=6379,password='raspberry')
+redis_conn = redis.Redis(host=os.environ['REDIS_HOST'],port=6379,password=os.environ['REDIS_PASSWORD'])
 print(redis_conn.ping())
 
 def on_message(mosq, obj, msg):
@@ -14,7 +17,7 @@ def on_message(mosq, obj, msg):
 if __name__ == '__main__':
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     client.on_message = on_message
-    client.connect('127.0.0.1')
+    client.connect(os.environ['MQTT_SERVER'])
     #client.connect('192.168.8.16')
     client.subscribe('501教室/LED燈',qos=2)
     client.loop_forever()
